eb007400608b58f4b56e116f862eead914a76a31
baezdev <baezdev@gmail.com>
2022-10-15 15:48:33 -0500

    feat🤠: complementos de formulario

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const FormButton = ({ text, icon }) => {
   return (
-    <button class="form__button form__button-primary">
-      <span class="form__button-text">{text}</span>
-      <span class="form__button-icon">
-        <i class={icon}></i>
+    <button className="form__button form__button-primary">
+      <span className="form__button-text">{text}</span>
+      <span className="form__button-icon">
+        <i className={icon}></i>
       </span>
     </button>
   );
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const FormInput = ({ type, name, placeholder, icon }) => {
+export const FormInput = ({ type, name, placeholder, icon, secIcon }) => {
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  if (name === "password") {
+    const showPassword = () => {
+      setIsShowPassword(!isShowPassword);
+    };
+
+    return (
+      <div className="form__input">
+        <span>
+          <i className="fa-solid fa-lock"></i>
+        </span>
+        <input
+          type={isShowPassword ? "text" : "password"}
+          name="password"
+          placeholder="Contraseña"
+        />
+        <button className="show__pass" type="button" onClick={showPassword}>
+          <i className="fa-regular fa-eye"></i>
+        </button>
+      </div>
+    );
+  }
+
   return (
-    <div class="form__input">
+    <div className="form__input">
       <span>
-        <i class={icon}></i>
+        <i className={icon}></i>
       </span>
       <input type={type} name={name} placeholder={placeholder} />
+      <span>
+        <i className={secIcon}></i>
+      </span>
     </div>
   );
 };
